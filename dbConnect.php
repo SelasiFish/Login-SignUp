9bd03a79cf38ee04205fb91dfbc694b4ef738e80
SelasiFish <fleagboselasi@gmail.com>
2016-08-15 18:37:07 +0300

    Delete dbConnect.php

--- dbConnect.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-	define('HOST','mysql12.000webhost.com');
-	define('User_id','root');
-	define('password','');
-	define('DB','a9949425_Felix');
-	
-	$con = mysqli_connect(HOST,User_id,pass,DB) or die('Unable to Connect');
