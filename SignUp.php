4b1074d37f8f5021e1cdf10a64652d562cefa85e
SelasiFish <fleagboselasi@gmail.com>
2016-08-17 12:16:37 +0300

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -1,14 +1,17 @@
 <?php
-require"connection.php";
 
+ require "connection.php";  
+
+    $User_id = $_POST["User_id"];
     $name = $_POST["name"];
     $country = $_POST["country"];
     $phone = $_POST["phone"];
     $password = $_POST["password"];
     $confirm_password = $_POST["confirm_password"];
     $verification_code = $_POST["verification_code"];
-
-  $sql_query = "insert into  values ('$name', '$country', '$phone', '$password', '$confirm_password', '$verification_code');";
+ 
+ 
+$sql_query = "insert into LoginSignUp values ('$User_id','$name', '$country', '$phone', '$password', '$confirm_password', '$verification_code');";
 
 if(mysqli_query($con,$sql_query))
 {
@@ -21,5 +24,7 @@ echo "Data insertion error".mysqli_error($con);
 
 
 ?>
+
+
 
 
