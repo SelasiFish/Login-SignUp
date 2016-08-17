57f621e1a1c9ca935dc84f8c9943243bf8630b3c
SelasiFish <fleagboselasi@gmail.com>
2016-08-17 03:39:55 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,40 @@
 <?php
+error_reporting (E_ALL ^ E_NOTICE);
+$form = "<form action='./login.php' method='post'>
+<table>
+<tr>
+    <td>phone:</td>
+    <td><input type='phone' name= 'phone'/></td>
+</tr>
+<tr>
+    <td>password:</td>
+    <td><input type='password' name= 'password'/></td>
+</tr>
+<tr>
+    <td></td>
+    <td><input type='submit' name= 'blogin' value='login'/></td>
 
-session _start();
+</tr>
+</table>
+</form>";
 
-<html xmlns="http://mytransportmyways.ml/">
+if ($_POST['blogin']){
+    $phone = $_POST['phone'];
+    $password = $_POST['password']; 
+
+   if ($phone){
+      if ($password){
+          echo "$phone = $phone <hr /> $form";
+      }
+      else
+         echo "You must enter your password. $form";
+   }
+   else
+       echo "You must enter your phone. $form";
+}
+else
+    echo $form;
+
 
 
 ?>
